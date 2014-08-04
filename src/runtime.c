e2d0bbdf296cb8ba3d149de3a874383dd94536fe
Trik SDK <trik@TrikSDK.(none)>
2014-08-04 16:36:07 +0400

    fifos correct name

--- src/runtime.c
+++ src/runtime.c
@@ -19,7 +19,7 @@ static const RuntimeConfig s_runtimeConfig = {
   .m_codecEngineConfig = { "dsp_server.xe674", "vidtranscode_cv" },
   .m_v4l2Config        = { "/dev/video0", 320, 240, V4L2_PIX_FMT_YUV422P },
   .m_fbConfig          = { "/dev/fb0" },
-  .m_rcConfig          = { "/run/line-sensor.in.fifo", "/run/line-sensor.out.fifo", true  }
+  .m_rcConfig          = { "/run/object-sensor.in.fifo", "/run/object-sensor.out.fifo", true  }
 };
 
 
